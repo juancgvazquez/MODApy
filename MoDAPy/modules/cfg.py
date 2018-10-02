83bb4e4345d2c363ed64c568375df6d99c1e7b3f
juancgvazquez <juancgvazquez@gmail.com>
2018-10-01 22:10:43 -0300

    fijo ruta ini

--- MoDAPy/modules/cfg.py
+++ MoDAPy/modules/cfg.py
@@ -1,7 +1,9 @@
 import configparser
+import os
 
 cfg = configparser.ConfigParser()
-cfg.read('config.ini')
+cfgpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini0)
+cfg.read(cfgpath)
 
 patientPath = cfg['PATHS']['PatientPath']
 panelsPath = cfg['PATHS']['PanelsPath']
