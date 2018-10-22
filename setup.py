9275f7fce0583b930c09f5a0cdaa2cfd29a93ab2
juancgvazquez <juancgvazquez@gmail.com>
2018-10-22 19:23:15 -0300

    fixed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rd:
 
 setuptools.setup(
 	name="MoDAPy",
-	version='0.0.6dev1',
+	version='0.0.7dev1',
 	author='Juan Carlos Vázquez',
 	author_email='juancgvazquez@gmail.com',
 	description='Package to perform several analysis on Multi-Omics Data',
