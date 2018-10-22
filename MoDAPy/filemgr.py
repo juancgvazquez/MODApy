b5038f0c7d46118c353aed0830b1ec920c9d6b1e
juancgvazquez <juancgvazquez@gmail.com>
2018-10-22 19:16:13 -0300

    fixed None

--- MoDAPy/filemgr.py
+++ MoDAPy/filemgr.py
@@ -8,7 +8,10 @@ def make_hyperlink(value:str,urltype):
 	if urltype == 'RSID':
 		url = "https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs={}"
 		if type(value) == str:
-			return '=HYPERLINK("%s", "%s")' % (url.format(value.split(',')[0]), value.split(',')[0])
+			if value == 'None':
+				return ''
+			else:
+				return '=HYPERLINK("%s", "%s")' % (url.format(value.split(',')[0]), value.split(',')[0])
 		else:
 			return '=HYPERLINK("%s", "%s")' % (url.format(value), value)
 	elif urltype == 'GENE':
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rd:
 
 setuptools.setup(
 	name="MoDAPy",
-	version='0.0.5dev40',
+	version='0.0.6',
 	author='Juan Carlos Vázquez',
 	author_email='juancgvazquez@gmail.com',
 	description='Package to perform several analysis on Multi-Omics Data',
