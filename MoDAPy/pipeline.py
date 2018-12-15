fc49adebe3c3adc4af25fcbfb98c0d9b4571a69d
juancgvazquez <juancgvazquez@gmail.com>
2018-12-15 18:47:03 -0300

    Changed pipeline to log.

--- MoDAPy/pipeline.py
+++ MoDAPy/pipeline.py
@@ -1,35 +1,37 @@
 import json
-import shlex
 import logging
+import shlex
 import subprocess
+
 from MoDAPy import cfg
 
+# TODO: restructure pipeline, being less open, so less prone to errors
 '''
 Class defined for each step of the Pipeline
 '''
 
 
 class PipeStep(object):
-	def __init__(self, name, command, subcommand, version, inputfile, outputfile, args):
-		self.name = name
-		self.command = command
-		self.subcommand = subcommand
-		self.version = version
-		self.inputfile = inputfile
-		self.outputfile = outputfile
-		self.args = args
+    def __init__(self, name, command, subcommand, version, inputfile, outputfile, args):
+        self.name = name
+        self.command = command
+        self.subcommand = subcommand
+        self.version = version
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+        self.args = args
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def __repr__(self):
-		return self.name
+    def __repr__(self):
+        return self.name
 
-	def stepinfo(self):
-		print('Name:', self.name)
-		print('Command', self.command + self.version, self.subcommand, self.args)
-		print('Input File:', self.inputfile)
-		print('Output File:', self.outputfile)
+    def stepinfo(self):
+        print('Name:', self.name)
+        print('Command', self.command + self.version, self.subcommand, self.args)
+        print('Input File:', self.inputfile)
+        print('Output File:', self.outputfile)
 
 
 '''
@@ -38,158 +40,158 @@ General Class for Pipelines
 
 
 class Pipeline(object):
-	def __init__(self, name, reference, url='', description=''):
-		self.name = name
-		self.url = url
-		self.description = description
-		self.reference = reference
-		self.required_files = []
-		self.steps = []
+    def __init__(self, name, reference, url='', description=''):
+        self.name = name
+        self.url = url
+        self.description = description
+        self.reference = reference
+        self.required_files = []
+        self.steps = []
 
-	def add_steps(self, step):
-		self.steps.append(step)
+    def add_steps(self, step):
+        self.steps.append(step)
 
-	def add_req_files(self, path):
-		self.required_files.append(path)
+    def add_req_files(self, path):
+        self.required_files.append(path)
 
-	'''
-	Class Method to build pipeline from loaded json,xml or yaml
-	'''
+    '''
+    Class Method to build pipeline from loaded json,xml or yaml
+    '''
 
-	@staticmethod
-	def _buildpipe(pipefile):
-		name = pipefile['INFO']['name']
-		url = pipefile['INFO']['url']
-		description = pipefile['INFO']['description']
-		reference = pipefile['INFO']['reference']
+    @staticmethod
+    def _buildpipe(pipefile):
+        name = pipefile['INFO']['name']
+        url = pipefile['INFO']['url']
+        description = pipefile['INFO']['description']
+        reference = pipefile['INFO']['reference']
 
-		newpipe = Pipeline(name, reference, url, description)
+        newpipe = Pipeline(name, reference, url, description)
 
-		for x in pipefile['INFO']['required_files']:
-			newpipe.add_req_files(x)
+        for x in pipefile['INFO']['required_files']:
+            newpipe.add_req_files(x)
 
-		for x in range(1, len(pipefile['STEPS']) + 1):
-			name = pipefile['STEPS'][str(x)]['name']
-			command = pipefile['STEPS'][str(x)]['command']
-			subcommand = pipefile['STEPS'][str(x)]['subcommand']
-			version = pipefile['STEPS'][str(x)]['version']
-			inputfile = pipefile['STEPS'][str(x)]['input']
-			outputfile = pipefile['STEPS'][str(x)]['output']
-			args = pipefile['STEPS'][str(x)]['args']
-			newstep = PipeStep(name, command, subcommand, version, inputfile, outputfile, args)
-			newpipe.add_steps(newstep)
+        for x in range(1, len(pipefile['STEPS']) + 1):
+            name = pipefile['STEPS'][str(x)]['name']
+            command = pipefile['STEPS'][str(x)]['command']
+            subcommand = pipefile['STEPS'][str(x)]['subcommand']
+            version = pipefile['STEPS'][str(x)]['version']
+            inputfile = pipefile['STEPS'][str(x)]['input']
+            outputfile = pipefile['STEPS'][str(x)]['output']
+            args = pipefile['STEPS'][str(x)]['args']
+            newstep = PipeStep(name, command, subcommand, version, inputfile, outputfile, args)
+            newpipe.add_steps(newstep)
 
-		return newpipe
+        return newpipe
 
-	'''
-	Class Method to create Pipeline from Json
-	'''
+    '''
+    Class Method to create Pipeline from Json
+    '''
 
-	@classmethod
-	def from_json(cls, jsonpath):
-		with open(jsonpath) as f:
-			pipefile = json.load(f)
+    @classmethod
+    def from_json(cls, jsonpath):
+        with open(jsonpath) as f:
+            pipefile = json.load(f)
 
-		builtpipe = cls._buildpipe(pipefile)
+        builtpipe = cls._buildpipe(pipefile)
 
-		return builtpipe
+        return builtpipe
 
-	'''
-	Method to run selected Pipeline on fastq files
-	'''
+    '''
+    Method to run selected Pipeline on fastq files
+    '''
 
-	def runpipeline(self, fastq1: str, fastq2=None):
-		patientname = fastq1.split('/')[-1].split('.')[0].split('_')[0]
-		ref = cfg.referencesPath + self.reference + '/' + self.reference + '.fa'
-		print('Running', self.name, 'pipeline on patient:', patientname)
-		# bool to check if first step
-		first = True
-		for step in self.steps:
-			# Checks if first step, we should input the exact input as inputfiles
-			if first is True:
-				first = False
-				if type(step.inputfile) == list:
-					if (fastq1 is not None) & (fastq2 is not None):
-						inputfile = fastq1 + ' ' + fastq2
-					else:
-						print('WARNING: This pipeline was designed for Pair End and you are running it as Single End')
-						inputfile = fastq1
-				elif type(step.inputfile) == str:
-					if (fastq1 is not None) & (fastq2 is not None):
-						print('WARNING: This pipeline was designed for Single End and you are running it as Pair End')
-						inputfile = fastq1 + ' ' + fastq2
-					else:
-						inputfile = fastq1
-				else:
-					return 'Error Parsing input file. It should be a string or list of strings.'
-			# If it's not first step, input depends on output of previous step + patientname
-			else:
-				inputfile = step.inputfile.replace('patientname', patientname)
+    def runpipeline(self, fastq1: str, fastq2=None):
+        patientname = fastq1.split('/')[-1].split('.')[0].split('_')[0]
+        ref = cfg.referencesPath + self.reference + '/' + self.reference + '.fa'
+        print('Running', self.name, 'pipeline on patient:', patientname)
+        # bool to check if first step
+        first = True
+        for step in self.steps:
+            # Checks if first step, we should input the exact input as inputfiles
+            if first is True:
+                first = False
+                if type(step.inputfile) == list:
+                    if (fastq1 is not None) & (fastq2 is not None):
+                        inputfile = fastq1 + ' ' + fastq2
+                    else:
+                        print('WARNING: This pipeline was designed for Pair End and you are running it as Single End')
+                        inputfile = fastq1
+                elif type(step.inputfile) == str:
+                    if (fastq1 is not None) & (fastq2 is not None):
+                        print('WARNING: This pipeline was designed for Single End and you are running it as Pair End')
+                        inputfile = fastq1 + ' ' + fastq2
+                    else:
+                        inputfile = fastq1
+                else:
+                    return 'Error Parsing input file. It should be a string or list of strings.'
+            # If it's not first step, input depends on output of previous step + patientname
+            else:
+                inputfile = step.inputfile.replace('patientname', patientname)
 
-			# replaces patient name in outputfiles
-			if type(step.outputfile) == str:
-				outputfile = step.outputfile.replace('patientname', patientname)
-			else:
-				return 'Error Parsing output file. It should be a string.'
+            # replaces patient name in outputfiles
+            if type(step.outputfile) == str:
+                outputfile = step.outputfile.replace('patientname', patientname)
+            else:
+                return 'Error Parsing output file. It should be a string.'
 
-			print(step.name)
-			args = step.args.replace('patientname', patientname).replace('reference', ref)
-			cmdver = step.version.replace('.', '_')
-			if 'GATK' in step.command or 'picard' in step.command:
-				cmd = 'java -jar ' + cfg.binPath + step.command + '/' + step.command + '_' + cmdver \
-					  + '.jar ' + step.subcommand
-			else:
-				cmd = cfg.binPath + step.command + '/' + step.command + '_' + cmdver + ' ' + step.subcommand
+            print(step.name)
+            args = step.args.replace('patientname', patientname).replace('reference', ref)
+            cmdver = step.version.replace('.', '_')
+            if 'GATK' in step.command or 'picard' in step.command:
+                cmd = 'java -jar ' + cfg.binPath + step.command + '/' + step.command + '_' + cmdver \
+                      + '.jar ' + step.subcommand
+            else:
+                cmd = cfg.binPath + step.command + '/' + step.command + '_' + cmdver + ' ' + step.subcommand
 
-			cmdstr = cmd + ' ' + args + ' ' + ' ' + inputfile + ' ' + outputfile
+            cmdstr = cmd + ' ' + args + ' ' + ' ' + inputfile + ' ' + outputfile
 
-			cmd = shlex.split(cmdstr)
+            cmd = shlex.split(cmdstr)
 
-			# logging stuff
-			formatter2 = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-			logging.basicConfig(level=logging.DEBUG, filename='log', format=formatter2)
-			console = logging.StreamHandler()
-			console.setLevel(logging.INFO)
-			formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-			console.setFormatter(formatter)
-			logging.getLogger('').addHandler(console)
-			# done logging config
-			logging.info('Subprocess: ' + cmdstr)
-			try:
-				if any("bwa" in s for s in cmd):
-					output = cmd[-1]
-					del cmd[-1]
-					with open(output, 'w+') as bwaout:
-						cmdrun = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=bwaout, universal_newlines=True)
-				else:
-					cmdrun = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
-											  universal_newlines=True)
-				out, err = cmdrun.communicate()
-				if out is not None:
-					logging.debug('Output: ' + out.strip())
-				if err is not None:
-					logging.debug('Stderr: ' + err.strip())
-				if cmdrun.returncode != 0:
-					logging.error('Subprocess failed with error code: ' + str(cmdrun.returncode))
-					logging.error('Check log for more details')
-					exit(cmdrun.returncode)
+            # logging stuff
+            formatter2 = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            logging.basicConfig(level=logging.DEBUG, filename='log', format=formatter2)
+            console = logging.StreamHandler()
+            console.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+            console.setFormatter(formatter)
+            logging.getLogger('').addHandler(console)
+            # done logging config
+            logging.info('Subprocess: ' + cmdstr)
+            try:
+                if any("bwa" in s for s in cmd):
+                    output = cmd[-1]
+                    del cmd[-1]
+                    with open(output, 'w+') as bwaout:
+                        cmdrun = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=bwaout, universal_newlines=True)
+                else:
+                    cmdrun = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+                                              universal_newlines=True)
+                out, err = cmdrun.communicate()
+                if out is not None:
+                    logging.debug('Output: ' + out.strip())
+                if err is not None:
+                    logging.debug('Stderr: ' + err.strip())
+                if cmdrun.returncode != 0:
+                    logging.error('Subprocess failed with error code: ' + str(cmdrun.returncode))
+                    logging.error('Check log for more details')
+                    exit(cmdrun.returncode)
 
-			except (OSError, subprocess.CalledProcessError) as exception:
-				logging.debug('Subprocess failed')
-				logging.debug('Exception ocurred: ' + str(exception))
-				logging.info('There was an error when running the pipeline. Please check logs for more info')
-				exit(1)
-			else:
-				logging.info('Subprocess finished')
+            except (OSError, subprocess.CalledProcessError) as exception:
+                logging.debug('Subprocess failed')
+                logging.debug('Exception ocurred: ' + str(exception))
+                logging.info('There was an error when running the pipeline. Please check logs for more info')
+                exit(1)
+            else:
+                logging.info('Subprocess finished')
 
-	'''
-	Method to print Pipeline Info
-	'''
+    '''
+    Method to print Pipeline Info
+    '''
 
-	def pipelineinfo(self):
-		print('Name:', self.name)
-		print('Reference:', self.reference)
-		print('URL:', self.url)
-		print('Description:', self.description)
-		print('Additional Files Required:', self.required_files)
-		print('Steps:', self.steps)
+    def pipelineinfo(self):
+        print('Name:', self.name)
+        print('Reference:', self.reference)
+        print('URL:', self.url)
+        print('Description:', self.description)
+        print('Additional Files Required:', self.required_files)
+        print('Steps:', self.steps)
--- MoDAPy/vcfmgr.py
+++ MoDAPy/vcfmgr.py
@@ -1,116 +1,118 @@
 from collections import OrderedDict
+
 import cyvcf2
 import pandas as pd
 
 
 class ParsedVCF(pd.DataFrame):
 
-	@staticmethod
-	def _divide(x, y):
-		try:
-			return float(x) / y
-		except:
-			return x
+    @staticmethod
+    def _divide(x, y):
+        try:
+            return float(x) / y
+        except:
+            return x
 
-	@property
-	def _constructor(self):
-		return ParsedVCF
+    @property
+    def _constructor(self):
+        return ParsedVCF
 
-	@classmethod
-	def from_vcf(cls, vcf):
-		try:
-			pVCF = cyvcf2.Reader(vcf)
-		except:
-			print('error loading vcf')
-			exit(1)
+    @classmethod
+    def from_vcf(cls, vcf):
+        try:
+            pVCF = cyvcf2.Reader(vcf)
+        except:
+            print('error loading vcf')
+            exit(1)
 
-		variantsDict = OrderedDict()
-		for variant in pVCF:
-			variantsDict[variant.CHROM + '_' + str(variant.POS) + '_' + variant.REF + '_' + ','.join(variant.ALT)] = {
-				'ID': variant.ID, 'QUAL': variant.QUAL, 'FILTER': variant.FILTER}
-			variantsDict[
-				variant.CHROM + '_' + str(variant.POS) + '_' + variant.REF + '_' + ','.join(variant.ALT)].update(
-				{k: v for (k, v) in variant.INFO})
+        variantsDict = OrderedDict()
+        for variant in pVCF:
+            variantsDict[variant.CHROM + '_' + str(variant.POS) + '_' + variant.REF + '_' + ','.join(variant.ALT)] = {
+                'ID': variant.ID, 'QUAL': variant.QUAL, 'FILTER': variant.FILTER}
+            variantsDict[
+                variant.CHROM + '_' + str(variant.POS) + '_' + variant.REF + '_' + ','.join(variant.ALT)].update(
+                {k: v for (k, v) in variant.INFO})
 
-		df1 = pd.DataFrame.from_dict(variantsDict, orient='index')
-		if 'ANN' in df1.columns:
-			anndf = df1['ANN']
-			annhead = pVCF.get_header_type('ANN')['Description'].strip('"Functional annotations: \'"')
-			annheaderlist = [x.strip() for x in annhead.split('|')]
-			anndf = anndf.str.split(',', expand=True).stack()
-			anndf = anndf.str.split('|', expand=True)
-			anndf.columns = annheaderlist
-			df1.drop(columns=['ANN'], inplace=True)
-			anndf.index = anndf.index.droplevel(1)
-			vcfdf = df1.join(anndf, how='inner')
-		else:
-			vcfdf = df1
+        df1 = pd.DataFrame.from_dict(variantsDict, orient='index')
+        if 'ANN' in df1.columns:
+            anndf = df1['ANN']
+            annhead = pVCF.get_header_type('ANN')['Description'].strip('"Functional annotations: \'"')
+            annheaderlist = [x.strip() for x in annhead.split('|')]
+            anndf = anndf.str.split(',', expand=True).stack()
+            anndf = anndf.str.split('|', expand=True)
+            anndf.columns = annheaderlist
+            df1.drop(columns=['ANN'], inplace=True)
+            anndf.index = anndf.index.droplevel(1)
+            vcfdf = df1.join(anndf, how='inner')
+        else:
+            vcfdf = df1
 
-		vcfdf.index = vcfdf.index.str.split('_', expand=True)
-		vcfdf.index.names = ['CHROM', 'POS', 'REF', 'ALT']
-		vcfdf.columns = vcfdf.columns.str.upper()
-		if 'HOM' in vcfdf.columns:
-			vcfdf['HOM'] = vcfdf['HOM'].map({True: 'HOM'})
-			vcfdf.HOM.fillna('HET', inplace=True)
-			vcfdf.rename(columns={'HOM': 'ZIGOSITY'}, inplace=True)
-		if 'ESP6500_MAF' in vcfdf.columns:
-			vcfdf[['ESP6500_MAF_EA', 'ESP6500_MAF_AA', 'ESP6500_MAF_ALL']] = vcfdf['ESP6500_MAF'].str.split(',',
-																											expand=True)
-			vcfdf['ESP6500_MAF_EA'] = vcfdf['ESP6500_MAF_EA'].apply(cls._divide, args=(100,))
-			vcfdf['ESP6500_MAF_AA'] = vcfdf['ESP6500_MAF_AA'].apply(cls._divide, args=(100,))
-			vcfdf['ESP6500_MAF_ALL'] = vcfdf['ESP6500_MAF_ALL'].apply(cls._divide, args=(100,))
-			vcfdf.drop(columns=['ESP6500_MAF'], inplace=True)
-		if 'ESP6500_PH' in vcfdf.columns:
-			vcfdf[['PolyPhen_Pred', 'PolyPhen_Score']] = vcfdf['ESP6500_PH'].str.split(':', 1, expand=True)
-			vcfdf['PolyPhen_Pred'] = vcfdf['PolyPhen_Pred'].str.strip('.').str.strip('.,')
-			vcfdf['PolyPhen_Score'] = vcfdf['PolyPhen_Score'].str.split(',').str[0]
-			vcfdf.drop(columns=['ESP6500_PH'], inplace=True)
-		vcfdf.rename(columns={'ANNOTATION': 'EFFECT', 'ANNOTATION_IMPACT': 'PUTATIVE_IMPACT'}, inplace=True)
-		result = vcfdf.pipe(ParsedVCF)
-		try:
-			result.name = pVCF.samples[0]
-		except:
-			result.name = vcf.split('/')[-1]
+        vcfdf.index = vcfdf.index.str.split('_', expand=True)
+        vcfdf.index.names = ['CHROM', 'POS', 'REF', 'ALT']
+        vcfdf.columns = vcfdf.columns.str.upper()
+        if 'HOM' in vcfdf.columns:
+            vcfdf['HOM'] = vcfdf['HOM'].map({True: 'HOM'})
+            vcfdf.HOM.fillna('HET', inplace=True)
+            vcfdf.rename(columns={'HOM': 'ZIGOSITY'}, inplace=True)
+        if 'ESP6500_MAF' in vcfdf.columns:
+            vcfdf[['ESP6500_MAF_EA', 'ESP6500_MAF_AA', 'ESP6500_MAF_ALL']] = vcfdf['ESP6500_MAF'].str.split(',',
+                                                                                                            expand=True)
+            vcfdf['ESP6500_MAF_EA'] = vcfdf['ESP6500_MAF_EA'].apply(cls._divide, args=(100,))
+            vcfdf['ESP6500_MAF_AA'] = vcfdf['ESP6500_MAF_AA'].apply(cls._divide, args=(100,))
+            vcfdf['ESP6500_MAF_ALL'] = vcfdf['ESP6500_MAF_ALL'].apply(cls._divide, args=(100,))
+            vcfdf.drop(columns=['ESP6500_MAF'], inplace=True)
+        if 'ESP6500_PH' in vcfdf.columns:
+            vcfdf[['PolyPhen_Pred', 'PolyPhen_Score']] = vcfdf['ESP6500_PH'].str.split(':', 1, expand=True)
+            vcfdf['PolyPhen_Pred'] = vcfdf['PolyPhen_Pred'].str.strip('.').str.strip('.,')
+            vcfdf['PolyPhen_Score'] = vcfdf['PolyPhen_Score'].str.split(',').str[0]
+            vcfdf.drop(columns=['ESP6500_PH'], inplace=True)
+        vcfdf.rename(columns={'ANNOTATION': 'EFFECT', 'ANNOTATION_IMPACT': 'PUTATIVE_IMPACT', 'ID': 'RSID'},
+                     inplace=True)
+        result = vcfdf.pipe(ParsedVCF)
+        try:
+            result.name = pVCF.samples[0]
+        except:
+            result.name = vcf.split('/')[-1]
 
-		return result
+        return result
 
-	def duos(self, vcf2):
-		vcf2df = ParsedVCF.from_vcf(vcf2)
-		AyB = self.loc[self.index.isin(vcf2df.index)].copy()
-		A = self.loc[~self.index.isin(vcf2df.index)].copy()
-		B = vcf2df.loc[~vcf2df.index.isin(self.index)].copy()
-		AyB['DUOS'] = ':'.join([self.name, vcf2df.name])
-		A['DUOS'] = self.name
-		B['DUOS'] = vcf2df.name
-		df_final = pd.concat([A, B, AyB], sort=False)
-		droplist = ['AC', 'SAMPLES_AF', 'AN', 'DP', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'QD', 'SOR', 'DBSNPBUILDID']
-		df_final = df_final.drop(columns=droplist)
-		df_final.name = ':'.join([self.name, vcf2df.name])
-		return df_final
+    def duos(self, vcf2):
+        vcf2df = ParsedVCF.from_vcf(vcf2)
+        AyB = self.loc[self.index.isin(vcf2df.index)].copy()
+        A = self.loc[~self.index.isin(vcf2df.index)].copy()
+        B = vcf2df.loc[~vcf2df.index.isin(self.index)].copy()
+        AyB['DUOS'] = ':'.join([self.name, vcf2df.name])
+        A['DUOS'] = self.name
+        B['DUOS'] = vcf2df.name
+        df_final = pd.concat([A, B, AyB], sort=False)
+        droplist = ['AC', 'SAMPLES_AF', 'AN', 'DP', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'QD', 'SOR', 'DBSNPBUILDID']
+        df_final = df_final.drop(columns=droplist)
+        df_final.name = ':'.join([self.name, vcf2df.name])
+        return df_final
 
-	def trios(self, vcf2, vcf3):
+    def trios(self, vcf2, vcf3):
 
-		vcf2df = ParsedVCF.from_vcf(vcf2)
-		print(vcf2df.name)
-		vcf3df = ParsedVCF.from_vcf(vcf3)
-		print(vcf3df.name)
-		A = self.loc[(~self.index.isin(vcf2df.index)) & (~self.index.isin(vcf3df.index))].copy()
-		A['TRIOS'] = self.name
-		B = vcf2df.loc[(~vcf2df.index.isin(self.index)) & (~vcf2df.index.isin(vcf3df.index))].copy()
-		B['TRIOS'] = vcf2df.name
-		C = vcf3df.loc[(~vcf3df.index.isin(self.index)) & (~vcf3df.index.isin(vcf2df.index))].copy()
-		C['TRIOS'] = vcf3df.name
-		ABC = self.loc[(self.index.isin(vcf2df.index)) & (self.index.isin(vcf3df.index))].copy()
-		ABC['TRIOS'] = ':'.join([self.name, vcf2df.name, vcf3df.name])
-		AB = self.loc[(self.index.isin(vcf2df.index)) & (~self.index.isin(vcf3df.index))].copy()
-		AB['TRIOS'] = ':'.join([self.name, vcf2df.name])
-		AC = self.loc[(self.index.isin(vcf3df.index)) & (~self.index.isin(vcf2df.index))].copy()
-		AC['TRIOS'] = ':'.join([self.name, vcf3df.name])
-		BC = vcf2df.loc[(vcf2df.index.isin(vcf3df.index)) & (~vcf2df.index.isin(self.index))].copy()
-		BC['TRIOS'] = ':'.join([vcf2df.name, vcf3df.name])
-		dfs = [A, B, AB, C, AC, BC, ABC]
-		df_final = pd.concat(dfs, sort=False)
-		droplist = ['AC', 'SAMPLES_AF', 'AN', 'DP', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'QD', 'SOR', 'DBSNPBUILDID']
-		df_final = df_final.drop(columns=droplist)
-		df_final.name = ':'.join([self.name, vcf2df.name, vcf3df.name])
-		return df_final
+        vcf2df = ParsedVCF.from_vcf(vcf2)
+        print(vcf2df.name)
+        vcf3df = ParsedVCF.from_vcf(vcf3)
+        print(vcf3df.name)
+        A = self.loc[(~self.index.isin(vcf2df.index)) & (~self.index.isin(vcf3df.index))].copy()
+        A['TRIOS'] = self.name
+        B = vcf2df.loc[(~vcf2df.index.isin(self.index)) & (~vcf2df.index.isin(vcf3df.index))].copy()
+        B['TRIOS'] = vcf2df.name
+        C = vcf3df.loc[(~vcf3df.index.isin(self.index)) & (~vcf3df.index.isin(vcf2df.index))].copy()
+        C['TRIOS'] = vcf3df.name
+        ABC = self.loc[(self.index.isin(vcf2df.index)) & (self.index.isin(vcf3df.index))].copy()
+        ABC['TRIOS'] = ':'.join([self.name, vcf2df.name, vcf3df.name])
+        AB = self.loc[(self.index.isin(vcf2df.index)) & (~self.index.isin(vcf3df.index))].copy()
+        AB['TRIOS'] = ':'.join([self.name, vcf2df.name])
+        AC = self.loc[(self.index.isin(vcf3df.index)) & (~self.index.isin(vcf2df.index))].copy()
+        AC['TRIOS'] = ':'.join([self.name, vcf3df.name])
+        BC = vcf2df.loc[(vcf2df.index.isin(vcf3df.index)) & (~vcf2df.index.isin(self.index))].copy()
+        BC['TRIOS'] = ':'.join([vcf2df.name, vcf3df.name])
+        dfs = [A, B, AB, C, AC, BC, ABC]
+        df_final = pd.concat(dfs, sort=False)
+        droplist = ['AC', 'SAMPLES_AF', 'AN', 'DP', 'FS', 'MLEAC', 'MLEAF', 'MQ', 'QD', 'SOR', 'DBSNPBUILDID']
+        df_final = df_final.drop(columns=droplist)
+        df_final.name = ':'.join([self.name, vcf2df.name, vcf3df.name])
+        return df_final
